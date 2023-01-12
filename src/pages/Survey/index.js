089aa8f6d80ce420dd34b53eb59d56e9c98a1431
eka-martin <99816833+eka-martin@users.noreply.github.com>
2023-01-12 17:37:46 +0100

    hook useTheme

--- src/pages/Survey/index.js
+++ src/pages/Survey/index.js
@@ -1,10 +1,11 @@
-import { useState, useEffect, useContext } from 'react'
+import { useContext } from 'react'
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import colors from '../../utils/style/colors'
 import { Loader } from '../../utils/style/Atoms'
 import { SurveyContext } from '../../utils/context'
+import { useFetch, useTheme } from '../../utils/hooks'
 
 const SurveyContainer = styled.div`
   display: flex;
@@ -15,21 +16,24 @@ const SurveyContainer = styled.div`
 const QuestionTitle = styled.h2`
   text-decoration: underline;
   text-decoration-color: ${colors.primary};
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
 `
 
 const QuestionContent = styled.span`
   margin: 30px;
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
 `
 
 const LinkWrapper = styled.div`
   padding-top: 30px;
   & a {
-    color: black;
+    color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
   }
   & a:first-of-type {
     margin-right: 20px;
   }
 `
+
 const ReplyBox = styled.button`
   border: none;
   height: 100px;
@@ -37,7 +41,9 @@ const ReplyBox = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: ${colors.backgroundLight};
+  background-color: ${({ theme }) =>
+    theme === 'light' ? colors.backgroundLight : colors.backgroundDark};
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
   border-radius: 30px;
   cursor: pointer;
   box-shadow: ${(props) =>
@@ -53,17 +59,19 @@ const ReplyBox = styled.button`
 const ReplyWrapper = styled.div`
   display: flex;
   flex-direction: row;
-  `
+`
 
 function Survey() {
   const { questionNumber } = useParams()
   const questionNumberInt = parseInt(questionNumber)
   const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1
   const nextQuestionNumber = questionNumberInt + 1
-  const [surveyData, setSurveyData] = useState({})
-  const [isDataLoading, setDataLoading] = useState(false)
+  const { theme } = useTheme()
+
   const { answers, saveAnswers } = useContext(SurveyContext)
-  const [error, setError] = useState(false)
+  
+  const { data, isLoading, error } = useFetch(`http://localhost:8000/survey`)
+  const surveyData = data?.surveyData
 
   function saveReply(answer) {
     saveAnswers({ [questionNumber]: answer })
@@ -82,48 +90,40 @@ function Survey() {
   //   )
   // }, [])
 
-  useEffect(() => {
-    async function fetchSurvey() {
-      setDataLoading(true)
-      try {
-        const response = await fetch(`http://localhost:8000/survey`)
-        const { surveyData } = await response.json()
-        setSurveyData(surveyData)
-      } catch (error) {
-        console.log('===== error =====', error)
-      }
-      finally {
-        setDataLoading(false)
-      }
+    if (error) {
+
+    return <span>Il y a un problème</span>
+    
     }
-    fetchSurvey()
-  }, [])
 
   return (
+    
     <SurveyContainer>
-      <QuestionTitle>Question {questionNumber}</QuestionTitle>
-      {isDataLoading ? (
+      <QuestionTitle theme={theme}>Question {questionNumber}</QuestionTitle>
+      {isLoading ? (
         <Loader />
       ) : (
-        <QuestionContent>{surveyData[questionNumber]}</QuestionContent>
+        <QuestionContent theme={theme}> {surveyData[questionNumber]}</QuestionContent>
       )}
       <ReplyWrapper>
         <ReplyBox
           onClick={() => saveReply(true)}
           isSelected={answers[questionNumber] === true}
+          theme={theme}
         >
           Oui
         </ReplyBox>
         <ReplyBox
           onClick={() => saveReply(false)}
           isSelected={answers[questionNumber] === false}
+          theme={theme}
         >
           Non
         </ReplyBox>
       </ReplyWrapper>
-      <LinkWrapper>
+      <LinkWrapper theme={theme}>
         <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
-        {surveyData[questionNumberInt + 1] ? (
+        {surveyData && surveyData[questionNumberInt + 1] ? (
           <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
         ) : (
           <Link to="/results">Résultats</Link>
---
+++ src/utils/hooks/index.js
@@ -0,0 +1,36 @@
+import { useState, useEffect, useContext } from 'react'
+import { ThemeContext } from '../context'
+
+export function useFetch(url) {
+
+    const [data, setData] = useState({})
+    const [isLoading, setLoading] = useState(true)
+    const [error, setError] = useState(false)
+
+
+    useEffect(() => {
+        if (!url) return
+        setLoading(true)
+        async function fetchData() {
+            try {
+                const response = await fetch(url)
+                const data = await response.json()
+                setData(data)
+            } catch (err) {
+                console.log(err)
+                setError(true)
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchData()
+    }, [url])
+    return { isLoading, data, error }
+}
+
+//-------------------------------------------
+
+export function useTheme() {
+    const { theme, toggleTheme } = useContext(ThemeContext)
+    return { theme, toggleTheme }
+  }
