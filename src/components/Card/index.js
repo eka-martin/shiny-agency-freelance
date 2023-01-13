d99a62fc31be69c996c2a81e98500d33f45804ad
eka-martin <99816833+eka-martin@users.noreply.github.com>
2023-01-13 19:35:03 +0100

    isFavorite

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,36 +1,59 @@
+import { useState } from 'react'
 import PropTypes from 'prop-types';
 import DefaultPicture from '../../assets/profile.png'
 import styled from 'styled-components';
 import colors from '../../utils/style/colors';
+import { useTheme } from '../../utils/hooks'
 
 const CardLabel = styled.span`
-color: #5843e4;
-font-size: 22px;
-font-weight: bold;`
-const CardImage = styled.img`
-    height: 80px;
-    width: 80px;
-    border-radius: 50%;
+  color: ${({ theme }) => (theme === 'light' ? colors.primary : '#ffffff')};
+  font-size: 22px;
+  font-weight: normal;
+  padding-left: 15px;
 `
+
+const CardTitle = styled.div`
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
+  font-size: 22px;
+  font-weight: normal;
+  align-self: center;
+  height: 25px;
+  display: flex;
+  align-items: center;
+`
+
+const CardImage = styled.img`
+  height: 150px;
+  width: 150px;
+  align-self: center;
+  border-radius: 50%;
+`
+
 const CardWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    padding: 15px;
-    background-color: ${colors.backgroundLight};
-    border-radius: 30px;
-    width: 350px;
-    transition: 200ms;
-    &:hover {
-        cursor: pointer;
-        box-shadow: 2px 2px 10px #e2e3e9;
-    }
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  padding: 15px;
+  background-color: ${({ theme }) =>
+        theme === 'light' ? colors.backgroundLight : colors.backgroundDark};
+  border-radius: 30px;
+  width: 300px;
+  height: 300px;
+  &:hover {
+    cursor: pointer;
+  }
 `
 function Card({ label, title, picture }) {
+    const { theme } = useTheme()
+    const [isFavorite, setIsFavorite] = useState(false)
+    const star = isFavorite ? '⭐️' : ''
     return (
-        <CardWrapper>
-            <CardLabel>{label}</CardLabel>
-            <CardImage src={picture} alt="freelance" height={80} width={80} />
-            <span>{title}</span>
+        <CardWrapper theme={theme} onClick={() => setIsFavorite(!isFavorite)}>
+            <CardLabel theme={theme}>{label}</CardLabel>
+            <CardImage src={picture} alt="freelance" />
+            <CardTitle theme={theme}>
+                {star} {title} {star}
+            </CardTitle>
         </CardWrapper>
     )
 }
